4e3c81626d8fbc01a68e1b578d5bba350c8fcb8a
Jim Hester <jim.hester@rstudio.com>
2021-08-05 00:00:02 +0000

    version 1.1.0

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,10 @@
+# archive 1.1.0
+
+* `archive_extract()` gains a `strip_components` argument to strip leading pathname components if desired (#27)
+
+* `archive()`, `archive_read()`, `archive_extract()` and `file_read()` now all accept R connections in addition to file paths.
+  This allows you to do things like read remote archives with a `url()` or `curl::curl()` connection like you would a file on disk (#7)
+
 # archive 1.0.2
 
 * skip tests failing on non-UTF-8 systems
--- tests/testthat/test-archive_extract.R
+++ tests/testthat/test-archive_extract.R
@@ -5,33 +5,44 @@ describe("archive_extract", {
     a <- archive(data_file)
     d <- tempfile()
     on.exit(unlink(d, recursive = TRUE))
-    archive_extract(a, d)
+    archive_extract(data_file, d)
 
     f <- list.files(d, full.names = TRUE)
     expect_equal(length(f), 3)
     expect_equal(file.size(f), a[order(a[["path"]]), ][["size"]])
   })
-  it("extracts given files in the archive, indexed by number", {
-    a <- archive(data_file)
-
+  it("extracts given files in the archive, indexed by integer", {
     d <- tempfile()
     on.exit(unlink(d, recursive = TRUE))
-    archive_extract(a, d, file = 1)
+    archive_extract(data_file, d, files = c(1L, 3L))
 
     f <- list.files(d, full.names = TRUE)
-    expect_equal(length(f), 1)
-    expect_equal(file.size(f), a[a$path == a$path[[1]], ][["size"]])
+    expect_equal(length(f), 2)
+
+    a <- archive(data_file)
+    expect_equal(sort(file.size(f)), sort(a[a$path %in% a$path[c(1, 3)], ][["size"]]))
+  })
+  it("extracts given files in the archive, indexed by double", {
+    d <- tempfile()
+    on.exit(unlink(d, recursive = TRUE))
+    archive_extract(data_file, d, files = c(1, 3))
+
+    f <- list.files(d, full.names = TRUE)
+    expect_equal(length(f), 2)
+
+    a <- archive(data_file)
+    expect_equal(sort(file.size(f)), sort(a[a$path %in% a$path[c(1, 3)], ][["size"]]))
   })
   it("extracts given files in the archive, indexed by name", {
-    a <- archive(data_file)
-
     d <- tempfile()
     on.exit(unlink(d, recursive = TRUE))
-    archive_extract(a, d, file = "mtcars.csv")
+    archive_extract(data_file, d, files = c("mtcars.csv", "iris.csv"))
 
     f <- list.files(d, full.names = TRUE)
-    expect_equal(length(f), 1)
-    expect_equal(file.size(f), a[a$path == "mtcars.csv", ][["size"]])
+    expect_equal(length(f), 2)
+
+    a <- archive(data_file)
+    expect_equal(sort(file.size(f)), sort(a[a$path %in% c("mtcars.csv", "iris.csv"), ][["size"]]))
   })
   it("can take options", {
     skip_on_os("windows")
@@ -49,6 +60,24 @@ describe("archive_extract", {
     Encoding(files) <- "UTF-8"
 
     expect_equal(files, filename)
+  })
+
+  it("can strip components if desired", {
+    in_dir <- tempfile()
+    out_dir <- tempfile()
+    on.exit(unlink(c(in_dir, out_dir), recursive = TRUE))
+
+    dir.create(file.path(in_dir, "foo/bar"), recursive = TRUE)
+
+    write.csv(iris, file.path(in_dir, "foo", "bar", "iris.csv"))
+    write.csv(mtcars, file.path(in_dir, "foo", "mtcars.csv"))
+
+    ar <- tempfile(fileext = ".tar")
+    archive_write_dir(ar, in_dir)
+
+    archive_extract(ar, out_dir, strip_components = 1)
+
+    expect_true(all(c("bar/iris.csv", "mtcars.csv") %in% list.files(out_dir, recursive = TRUE)))
   })
 })
 
